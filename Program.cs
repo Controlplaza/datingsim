1706f19e72047da1a529a2e8a4696e7712e85555
Ezekiel Tyus <EJTyus@student.fullsail.edu>
2020-02-13 17:41:42 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datingsimverz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dating simulator build: 0.5\t\n");
+            Console.ReadKey();  
+        }
+    }
+}
