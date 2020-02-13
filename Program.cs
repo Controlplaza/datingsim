b8249009b0dce5fb2d933adc6e47e5296a66c8c2
Ezekiel Tyus <EJTyus@student.fullsail.edu>
2020-02-13 17:58:57 -0500

    making the first writeline of the console

--- Program.cs
+++ Program.cs
@@ -10,8 +10,36 @@ namespace datingsimverz1
     {
         static void Main(string[] args)
         {
+            /*
+             main menu to start game
+
+            intro sequence saying hello giving you your bio
+
+
+             
+             
+             
+             */
+            bool sim = true;
+            Char firstanswr;
             Console.WriteLine("Dating simulator build: 0.5\t\n");
-            Console.ReadKey();  
+            Console.WriteLine("Start game\nstart:s\nquit:q");
+            firstanswr = Console.ReadKey().KeyChar;
+            if(firstanswr == 's' || firstanswr == 'S')
+            {
+                sim = true;
+            }
+            else if (firstanswr == 'q' || firstanswr == 'Q')
+            {
+                sim = false;
+            }
+            else
+            {
+                Console.WriteLine("\nthats not an authorized  answer");
+            }
+         
+            Console.ReadKey();
+            Console.ReadLine();
         }
     }
 }
